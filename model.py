85be16e2606bf504525149209324a7018bf654db
Gabe Maja <gabemaja10@gmail.com>
2021-11-20 12:22:02 +0200

    Updated Changes for EC2 Deployment

--- model.py
+++ model.py
@@ -58,7 +58,9 @@ def _preprocess_data(data):
     # ---------------------------------------------------------------
 
     # ----------- Replace this code with your own preprocessing steps --------
-    predict_vector = feature_vector_df[['Madrid_wind_speed','Bilbao_rain_1h','Valencia_wind_speed']]
+    #predict_vector = feature_vector_df[['Madrid_wind_speed','Bilbao_rain_1h','Valencia_wind_speed']]
+
+    predict_vector = feature_vector_df[['Seville_temp_max','Barcelona_wind_speed','Barcelona_pressure']]
     # ------------------------------------------------------------------------
 
     return predict_vector
